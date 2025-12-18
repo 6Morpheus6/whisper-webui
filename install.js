ca9eda9d7f5e36a4922a5c3967edeae46f09dbf8
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-12-18 22:06:55 +0100

    update

--- install.js
+++ install.js
@@ -1,10 +1,24 @@
 module.exports = {
+  requires: {
+    bundle: "ai",
+  },
   run: [
     {
       method: "shell.run",
       params: {
         message: [
           "git clone https://github.com/jhj0517/Whisper-WebUI app",
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",
+        path: "app",
+        message: [
+          "uv pip install -r requirements.txt",
+          "uv pip install gradio==5.34.0 numpy==1.26.4 hf-xet"
         ]
       }
     },
@@ -16,22 +30,6 @@ module.exports = {
           venv: "env",
           path: "app",
         }
-      }
-    },
-    {
-      method: "shell.run",
-      params: {
-        venv: "env",
-        path: "app",
-        message: [
-          "pip install -r requirements.txt"
-        ]
-      }
-    },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
       }
     }
   ]
